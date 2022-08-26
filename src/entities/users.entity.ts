044bdb7724be47cd6d0d1f6c4ba8d4d0e96f613c
Vinicius Rocha <vinicius.rocha.mobile@gmail.com>
2022-08-26 11:22:40 -0400

    :sparkles: feat: adjusting the entity

--- src/entities/users.entity.ts
+++ src/entities/users.entity.ts
@@ -3,14 +3,18 @@ import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateCol
 export class UsersEntity {
     @PrimaryGeneratedColumn('uuid')
     readonly id: string;
-    @Column({ name: 'usr_title', type: 'varchar', length: 255 })
-    title: string;
-    @Column({ name: 'usr_overview', type: 'text' })
-    overview: string;
-    @Column({ name: 'usr_release_date', type: 'date' })
-    release_date: Date;
-    @Column({ name: 'usr_is_adult', type: 'bool', default: false })
-    is_adult: boolean;
+    @Column({ name: 'usr_name', type: 'varchar', length: 255 })
+    name: string;
+    @Column({ name: 'usr_cpf', type: 'char', length: 11, unique: true })
+    cpf: string;
+    @Column({ name: 'usr_birthdate', type: 'date' })
+    birthdate: Date;
+    @Column({ name: 'usr_cellphone', type: 'char', length: 13, unique: true })
+    cellphone: string;
+    @Column({ name: 'usr_email', type: 'varchar', length: 255, unique: true, readonly: true })
+    email: string;
+    @Column({ name: 'usr_password', type: 'text' })
+    password: string;
     @CreateDateColumn({ name: 'usr_created_at' })
     readonly created_at: Date;
     @UpdateDateColumn({ name: 'usr_updated_at' })
