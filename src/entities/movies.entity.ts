35de80702a4445fb385fcdbb036eb68b69b83f43
Vinicius Rocha <vinicius.rocha.mobile@gmail.com>
2022-08-26 01:42:54 -0400

    :sparkles: feat: configuring the entity

--- src/entities/movies.entity.ts
+++ src/entities/movies.entity.ts
@@ -1,1 +1,25 @@
-export class MoviesEntity {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+@Entity('tbl_movies')
+export class MoviesEntity {
+  @PrimaryGeneratedColumn('uuid')
+  @Column({ name: 'mvi_title' })
+  readonly id: string;
+  @Column({ name: 'mvi_title', type: 'varchar', length: 255 })
+  title: string;
+  @Column({ name: 'mvi_overview', type: 'text' })
+  overview: string;
+  @Column({ name: 'mvi_release_date', type: 'date' })
+  release_date: Date;
+  @Column({ name: 'mvi_is_adult', type: 'bool', default: false })
+  is_adult: boolean;
+  @CreateDateColumn({ name: 'mvi_created_at' })
+  readonly created_at: Date;
+  @UpdateDateColumn({ name: 'mvi_updated_at' })
+  readonly updated_at: Date;
+}
--- src/modules/movies.module.ts
+++ src/modules/movies.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from 'src/controllers/movies.controller';
+import { MoviesEntity } from 'src/entities/movies.entity';
 @Module({
-  imports: [],
+  imports: [MoviesEntity],
   controllers: [MoviesController],
 })
 export class MoviesModule {}
