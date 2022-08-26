5f479009aa845f2131f76f59cefb80f2964eff50
Vinicius Rocha <vinicius.rocha.mobile@gmail.com>
2022-08-26 04:51:59 -0400

    :bug: fix: fix the base service's typescript and adding DTO

--- src/services/movies.service.ts
+++ src/services/movies.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { MoviesParamsDTO, MoviesPATCHBodyDTO, MoviesPOSTBodyDTO } from 'src/dtos/movies.dto';
 import { MoviesEntity } from 'src/entities/movies.entity';
 import { Repository } from 'typeorm';
 @Injectable()
@@ -8,19 +9,19 @@ export class MoviesService {
         @InjectRepository(MoviesEntity)
         private repository: Repository<MoviesEntity>,
     ) {}
-    async save(body) {
+    async save(data: MoviesPOSTBodyDTO): Promise<MoviesEntity> {
         return undefined;
     }
-    async getAll() {
+    async getAll(): Promise<MoviesEntity[]> {
         return undefined;
     }
-    async getOne(params) {
+    async getOne({ id }: MoviesParamsDTO): Promise<MoviesEntity> {
         return undefined;
     }
-    async update(params, body) {
+    async update({ id }: MoviesParamsDTO, data: MoviesPATCHBodyDTO): Promise<MoviesEntity> {
         return undefined;
     }
-    async delete(params) {
+    async delete({ id }: MoviesParamsDTO) {
         return undefined;
     }
 }
