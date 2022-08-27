4e17b7654d3092a104933b91dc4673fbffc35204
Vinicius Rocha <vinicius.rocha.mobile@gmail.com>
2022-08-27 17:14:21 -0400

    :sparkles: feat: adjusting TypeORMConfig for url connection

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,14 +1,8 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import 'dotenv/config';
 export const TypeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTGRES_PORT),
-  database: process.env.POSTGRES_DB,
-  synchronize: !!process.env.POSTGRES_SYNCHRONIZE
-    ? Boolean(process.env.POSTGRES_SYNCHRONIZE)
-    : false,
-  entities: ['dist/**/*.entity.js'],
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    synchronize: !!process.env.POSTGRES_SYNCHRONIZE ? Boolean(process.env.POSTGRES_SYNCHRONIZE) : false,
+    entities: ['dist/**/*.entity.js'],
 };
